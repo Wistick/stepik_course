12d2009fc3e70f6910a8e8a20036e12762cfeb55
Vadim Pogarsky <vadim.ska8@yandex.ru>
2021-03-04 15:18:19 +0300

    update for ALL

--- test_directory/wtf_test_1.6.py
+++ test_directory/wtf_test_1.6.py
@@ -7,17 +7,17 @@ driver = webdriver.Chrome(
     )
 
 try:
-    url = 'http://suninjuly.github.io/registration2.html'
-    driver.get(url)
-    driver.find_element_by_css_selector('.first_block > .form-group.first_class > input').send_keys('Name')
-    driver.find_element_by_css_selector('.first_block > .form-group.second_class > input').send_keys('Last name')
-    driver.find_element_by_css_selector('.first_block > .form-group.third_class > input').send_keys('email')
-    driver.find_element_by_css_selector('.second_block > .form-group.first_class > input').send_keys('77777777')
-    driver.find_element_by_css_selector('.second_block > .form-group.second_class > input').send_keys('address')
-    driver.find_element_by_css_selector('button[type="submit"]').click()
+    for i in range(1, 3):
+        driver.get(f'http://suninjuly.github.io/registration{i}.html')
+        driver.find_element_by_css_selector('.first_block > .form-group.first_class > input').send_keys('Name')
+        driver.find_element_by_css_selector('.first_block > .form-group.second_class > input').send_keys('Last name')
+        driver.find_element_by_css_selector('.first_block > .form-group.third_class > input').send_keys('email')
+        driver.find_element_by_css_selector('.second_block > .form-group.first_class > input').send_keys('77777777')
+        driver.find_element_by_css_selector('.second_block > .form-group.second_class > input').send_keys('address')
+        driver.find_element_by_css_selector('button[type="submit"]').click()
 
-    congratz = driver.find_element_by_css_selector('h1').text
-    assert 'Congratulations! You have successfully registered!' == congratz
+        congratz = driver.find_element_by_css_selector('h1').text
+        assert 'Congratulations! You have successfully registered!' == congratz
 
 except Exception as e:
     print(e)
